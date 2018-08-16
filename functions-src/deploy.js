04fd4411eeb6091652b6f9dbb4115a66c9a895b2
Phil Hawksworth <phil@netify.com>
2018-08-16 23:52:41 +0100

    debug

--- functions-src/deploy.js
+++ functions-src/deploy.js
@@ -3,17 +3,18 @@ var request = require('request');
 
 function buildResponse(request){
 
-  var intentName = request.intent.name;
-  var respondWith = "Ummm. I'm sorry, I don't quite understand."
+  // var intentName = request.intent.name;
+  // var respondWith = "Ummm. I'm sorry, I don't quite understand."
 
-  if(intentName == "deploy") {
-    request({
-      method: 'POST',
-      url: 'https://api.netlify.com/build_hooks/5b75f84e82d3f10467d27f41'
-    });
-    respondWith = "Ok, deploying hawks worx dot com, on Netlify";
-  }
+  // if(intentName == "deploy") {
+  //   request({
+  //     method: 'POST',
+  //     url: 'https://api.netlify.com/build_hooks/5b75f84e82d3f10467d27f41'
+  //   });
+  //   respondWith = "Ok, deploying hawks worx dot com, on Netlify";
+  // }
 
+  var respondWith = "Ok, deploying hawks worx dot com, on Netlify";
   var response = {
     "version": "0.1",
     "response": {
@@ -29,15 +30,20 @@ function buildResponse(request){
 
 
 export function handler(event, context, callback) {
-  if (event.request.type === 'LaunchRequest') {
-    console.log(event.request.type);
-    callback(null, buildResponse(event.request));
-  } else if (event.request.type === 'IntentRequest') {
-      console.log(event.request.type);
-      callback(null, buildResponse(event.request));
-  } else if (event.request.type === 'SessionEndedRequest') {
-      console.log(event.request.type);
-      onSessionEnded(event.request, event.session);
-      callback();
-  }
+
+  console.log('event :', event);
+
+  callback(null, buildResponse(event.request));
+
+  // if (event.request.type === 'LaunchRequest') {
+  //   console.log(event.request.type);
+  //   callback(null, buildResponse(event.request));
+  // } else if (event.request.type === 'IntentRequest') {
+  //     console.log(event.request.type);
+  //     callback(null, buildResponse(event.request));
+  // } else if (event.request.type === 'SessionEndedRequest') {
+  //     console.log(event.request.type);
+  //     onSessionEnded(event.request, event.session);
+  //     callback();
+  // }
 }
